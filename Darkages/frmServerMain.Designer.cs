b8383ab37ee41ea86ca4d629e987beba7470e316
Dean <dm8824163@gmail.com>
2018-01-03 01:29:01 +1000

    UI

--- Darkages/frmServerMain.Designer.cs
+++ Darkages/frmServerMain.Designer.cs
@@ -60,17 +60,15 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button5 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
@@ -82,7 +80,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1098, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -214,7 +212,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(365, 27);
+            this.groupBox1.Location = new System.Drawing.Point(789, 27);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(297, 114);
             this.groupBox1.TabIndex = 2;
@@ -248,9 +246,9 @@
             // 
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.propertyGrid1);
-            this.groupBox2.Location = new System.Drawing.Point(367, 141);
+            this.groupBox2.Location = new System.Drawing.Point(789, 147);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(297, 133);
+            this.groupBox2.Size = new System.Drawing.Size(297, 197);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Visible = false;
@@ -258,7 +256,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 94);
+            this.button1.Location = new System.Drawing.Point(8, 157);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 25);
             this.button1.TabIndex = 1;
@@ -275,7 +273,7 @@
             this.propertyGrid1.LineColor = System.Drawing.SystemColors.ControlDark;
             this.propertyGrid1.Location = new System.Drawing.Point(8, 19);
             this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(283, 72);
+            this.propertyGrid1.Size = new System.Drawing.Size(283, 132);
             this.propertyGrid1.TabIndex = 0;
             this.propertyGrid1.TabStop = false;
             this.propertyGrid1.ToolbarVisible = false;
@@ -285,7 +283,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(465, 350);
+            this.label1.Location = new System.Drawing.Point(389, 352);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 4;
@@ -299,15 +297,15 @@
             this.panel1.BackColor = System.Drawing.Color.Black;
             this.panel1.Location = new System.Drawing.Point(7, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(354, 309);
+            this.panel1.Size = new System.Drawing.Size(776, 315);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(509, 490);
+            this.button3.Location = new System.Drawing.Point(716, 462);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(153, 46);
+            this.button3.Size = new System.Drawing.Size(200, 58);
             this.button3.TabIndex = 5;
             this.button3.Text = "Start Server";
             this.button3.UseVisualStyleBackColor = true;
@@ -315,14 +313,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 344);
+            this.textBox1.Location = new System.Drawing.Point(7, 348);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(301, 22);
             this.textBox1.TabIndex = 6;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(314, 344);
+            this.button4.Location = new System.Drawing.Point(314, 347);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(47, 24);
             this.button4.TabIndex = 7;
@@ -333,7 +331,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(510, 467);
+            this.checkBox1.Location = new System.Drawing.Point(717, 439);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(154, 17);
             this.checkBox1.TabIndex = 8;
@@ -342,7 +340,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(611, 345);
+            this.button5.Location = new System.Drawing.Point(611, 347);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(51, 23);
             this.button5.TabIndex = 9;
@@ -354,28 +352,18 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::Darkages.Properties.Resources.darkages;
-            this.pictureBox1.Location = new System.Drawing.Point(7, 373);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 376);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(667, 198);
+            this.pictureBox1.Size = new System.Drawing.Size(1079, 198);
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = global::Darkages.Properties.Resources.mantissketch;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(367, 281);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(92, 87);
-            this.pictureBox2.TabIndex = 11;
-            this.pictureBox2.TabStop = false;
             // 
             // frmServerMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(674, 574);
+            this.ClientSize = new System.Drawing.Size(1098, 574);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button4);
@@ -387,7 +375,6 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBox2);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
@@ -403,7 +390,6 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -441,7 +427,6 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ToolStripMenuItem modifyToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
